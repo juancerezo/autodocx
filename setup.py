54c3fe7ff3d0d54acbcb6416ad1b58e27216d1a5
Juan Cerezo <jcerezotic@gmail.com>
2022-11-09 20:25:52 +0100

    first version

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name="autodocx",
+    version="0.1.0",
+    packages=find_namespace_packages(exclude=["build", "build.*"]),
+    install_requires=["click==8.1.3", "python-docx==0.8.11", "openpyxl==3.0.10"],
+    entry_points={
+        "console_scripts": [
+            "autodocx = autodocx:cli",
+        ],
+    },
+)
